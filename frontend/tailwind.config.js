65b59d0335a6c33e39c3a2c306eb145a0f3c4f0a
Meilika <meilika.dwi101619@students.unila.ac.id>
2022-06-09 17:40:32 +0700

    Install Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
