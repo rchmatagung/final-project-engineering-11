4c71be762e427152794a17a4e602e2b81e26dd11
Meilika <meilika.dwi101619@students.unila.ac.id>
2022-06-12 15:33:29 +0700

    feat: add article

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Articles from  "./components/Articles";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Articles />
     </div>
   );
 }
