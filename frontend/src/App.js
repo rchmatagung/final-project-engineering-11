39efbd50bcb14a29f9c4dcc363ba7a1a2e7e78b3
Meilika <meilika.dwi101619@students.unila.ac.id>
2022-06-12 15:39:55 +0700

    feat: add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Articles from  "./components/Articles";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Articles />
+      <Footer />
     </div>
   );
 }
