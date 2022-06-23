8daacb648802ae9dbe58172b5878ea4ce6388209
Meilika <meilika.dwi101619@students.unila.ac.id>
2022-06-24 02:23:47 +0700

    Merge feature/findmentor-component and update tailwindcss

---
+++ frontend/src/components/Listrole.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function Listrole() {
+  return (
+    <>
+    <h1 className="flex justify-center  text-4xl md:text-5xl lg:text-5xl font-bold">Role mentor</h1>
+    <div className=" grid grid-cols-4 gap-4">
+        <div>
+            <div className=" container mx-auto p-10 md:py-20 px-0 md:p-10 md:px-0">
+                <div className=" p-10 min-h-116 max-w-xs w-7xs bg-gray-200 rounded-xl text-gray-600 transform duration-500 hover:-translate-y-1 cursor-pointer">
+                    <div className="mt-5">
+                        <span className="text-xl">Frontend Engineer</span>
+                    </div>
+
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-700 hover:shadow-lg focus:bg-yellow-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-800 active:shadow-lg transition duration-150 ease-in-out">Find</button>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div className=" container mx-auto p-10 md:py-20 px-0 md:p-10 md:px-0">
+                <div className=" p-10 min-h-116 max-w-xs w-7xs bg-gray-200 rounded-xl text-gray-600 transform duration-500 hover:-translate-y-1 cursor-pointer">
+                    <div className="mt-5">
+                        <span className="text-xl">Frontend Engineer</span>
+                    </div>
+
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-700 hover:shadow-lg focus:bg-yellow-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-800 active:shadow-lg transition duration-150 ease-in-out">Find</button>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div className=" container mx-auto p-10 md:py-20 px-0 md:p-10 md:px-0">
+                <div className=" p-10 min-h-116 max-w-xs w-7xs bg-gray-200 rounded-xl text-gray-600 transform duration-500 hover:-translate-y-1 cursor-pointer">
+                    <div className="mt-5">
+                        <span className="text-xl">Frontend Engineer</span>
+                    </div>
+
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-700 hover:shadow-lg focus:bg-yellow-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-800 active:shadow-lg transition duration-150 ease-in-out">Find</button>
+                </div>
+            </div>
+        </div>
+            
+    </div>
+    
+
+    
+</>
+  )
+}
+
+export default Listrole
