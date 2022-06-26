18d52769e939a09e164fcd9fe60c7090fd40eaad
Meilika <meilika.dwi101619@students.unila.ac.id>
2022-06-26 15:49:02 +0700

    feat: user can book mentor

--- frontend/src/components/Listmentor.jsx
+++ frontend/src/components/Listmentor.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import api from '../api/api'
+import { useNavigate } from 'react-router-dom';
 
 export default function Listmentor() {
     const [mentors, setMentor] = useState([])
+    const nav = useNavigate();
 
-    const getUser = async () => {
+    const getMentor = async () => {
         try {
         await api.get('/user/mentor/mentorlist')
          .then((res) => {
@@ -17,16 +19,27 @@ export default function Listmentor() {
     };
     
     useEffect(() => {
-        getUser();
+        getMentor();
     }, []);
 
+    const bookMentor = async (id) => {
+      try {
+        await api.get(`/user/booking/mentor/${id}`)
+         .then((res) => {
+            nav("/profile")
+        })
+        } catch (error) {
+          console.log(error);
+        }
+    }
+
       return (
-        <div className="bg-gray-200 dark:bg-white-500">
-        <h1 className="flex justify-center  text-4xl md:text-5xl lg:text-5xl font-bold">Daftar Mentor</h1>
-        <div className=" bg-gray-200 flex justify-center dark:bg-white-500 flex flex-wrap items-center">
+        <div className="bg-gray-200">
+        <h1 className="flex justify-center text-4xl md:text-5xl lg:text-5xl font-bold">Daftar Mentor</h1>
+        <div className=" bg-gray-200 flex justify-center flex flex-wrap items-center">
           {mentors.map((mentor, index) => {
           return (
-          <div className="container max-w-lg bg-white rounded dark:bg-gray-800 shadow-lg transform duration-200 easy-in-out m-20 hover:scale-110"  key={index}>
+          <div className="container max-w-lg bg-white rounded hadow-lg transform duration-200 easy-in-out m-20 hover:scale-110" key={index}>
                   <div className="flex flex-wrap h-2/4 sm:h-64 overflow-hidden">
                       <img
                         className="w-full rounded-t"
@@ -45,16 +58,18 @@ export default function Listmentor() {
                     </div>
                     <div className="">
                       <div className="px-8 mb-8">
-                        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                         {mentor.name}
                         </h2>
-                        <p className="text-yellow-400 mt-2 dark:text-yellow-400">
+                        <p className="text-yellow-400 mt-2">
                         Mentor {mentor.skill}
                         </p>
-                        <p className="mt-2 text-gray-500 dark:text-gray-300">
+                        <p className="mt-2 text-gray-500">
                           {mentor.bio}
                         </p>
-                        <button onClick={() => window.location = ''} className="mt-5 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-yellow-500 rounded-lg hover:bg-yellow-700 focus:ring-4 focus:outline-none focus:ring-yellow-200 dark:bg-yellow-400 dark:hover:bg-yellow-600 dark:focus:ring-yellow-700">
+                        <button
+                        onClick={() => bookMentor(mentor.id)}
+                        className="mt-5 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-yellow-500 rounded-lg hover:bg-yellow-700 focus:ring-4 focus:outline-none focus:ring-yellow-200">
                           Buat Janji
                         </button>
                       </div>
