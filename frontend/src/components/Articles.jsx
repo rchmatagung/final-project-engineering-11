c7ffb98d4a014059e8d5417d47a8e5cc7ee61821
Meilika <meilika.dwi101619@students.unila.ac.id>
2022-06-26 23:19:44 +0700

    feat: Article Detail

--- frontend/src/components/Articles.jsx
+++ frontend/src/components/Articles.jsx
@@ -5,6 +5,7 @@ import api from '../api/api'
 function Articles() {
   const [articles, setArticles] = useState([])
   const [showModal, setShowModal] = useState(false);
+  const [articleDetail, setArticleDetail] = useState(null)
 
     const getArticle = async () => {
         try {
@@ -22,12 +23,16 @@ function Articles() {
     getArticle()
   }, [])
 
+  const handleArticleDetail = (article) => {
+    setShowModal(true)
+    setArticleDetail(article)
+  }
 
   return (
     <div className="container mx-auto px-8 py-5">
       <h2 className='text-center text-4xl md:text-5xl hover:text-yellow-500 font-bold'>Artikel</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 space-x-3 mt-10">
-        {articles.map((article) => {
+        {articles.map((article, index) => {
           return (
             <div className='mt-4 col-span-1 bg-gray-200 shadow-xl' key={article.id}>
               <img className="rounded-t-lg" src={Art} alt="Article Pic"/>
@@ -38,22 +43,16 @@ function Articles() {
                 </div>
                 <button
                   className="bg-yellow-500 hover:bg-yellow-700 text-white active:bg-yellow-600 font-bold hover:yellow-700 uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-2 mt-5 ease-linear transition-all duration-150"
-                  onClick={setShowModal(true)}>
+                  onClick={() => handleArticleDetail(article)}>
                   Read More
                 </button>
               {showModal  ? (
               <>
-              <div
-                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-              >
-                <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                  {/*content*/}
-                  <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                    {/*header*/}
+              <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                <div className="relative h-[600px] w-auto my-4 mx-auto max-w-3xl">
+                  <div className="border-0 rounded-lg shadow-lg w-full relative flex flex-col bg-white outline-none focus:outline-none">
                     <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                      <h3 className="text-3xl font-semibold">
-                        {article.title}
-                      </h3>
+                      <h3 className="text-3xl font-semibold">{articleDetail.title}</h3>
                       <button
                         className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                         onClick={() => setShowModal(false)}
@@ -63,13 +62,9 @@ function Articles() {
                         </span>
                       </button>
                     </div>
-                    {/*body*/}
                     <div className="relative p-6 flex-auto">
-                      <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                      {article.content}
-                      </p>
+                      <p className="my-4 text-slate-500 text-lg leading-relaxed"> {articleDetail.content}</p>
                     </div>
-                    {/*footer*/}
                     <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                     <button
                     className="bg-yellow-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
